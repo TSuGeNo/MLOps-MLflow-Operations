d46e17405db5799b30bcd2f940de05d3a4251c12
pradnyawant <pradnyawantgote100@gmail.com>
2025-10-01 01:19:30 +0530

    prepare base model stage added

--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -7,3 +7,15 @@ class DataIngestionConfig:
     source_URL: str = "https://drive.google.com/file/d/1z0mreUtRmR-P-magILsDR3T7M6IkGXtY/view?usp=sharing"
     local_data_file: Path = root_dir / "Chest-CT-Scan-data.zip"
     unzip_dir: Path = root_dir / "unzipped"
+
+
+@dataclass(frozen=True)
+class PrepareBaseModelConfig:
+    root_dir: Path
+    base_model_path: Path
+    updated_base_model_path: Path
+    params_image_size: list
+    params_learning_rate: float
+    params_include_top: bool
+    params_weights: str
+    params_classes: int
